a4367e6167bc537a9126d35e7055f5734673506c
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-04-30 21:07:42 +0300

    Tamamlandı.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reducer from "./reducers";
+import { BrowserRouter } from "react-router-dom";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
+//for toasting
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const depo0223 = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <>
-      <App />
-    </>
-  </BrowserRouter>
+  <Provider store={depo0223}>
+    <BrowserRouter>
+      <>
+        <ToastContainer />
+        <App />
+      </>
+    </BrowserRouter>
+  </Provider>
 );
--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,6 @@
-const s10chLocalStorageKey = "s10ch";
+import { NOT_EKLE, NOT_SIL } from "./actions";
+
+const s10chLocalStorageKey = "s10ch0223";
 
 const baslangicDegerleri = {
   notlar: [
@@ -24,6 +26,30 @@ function baslangicNotlariniGetir(key) {
   if (eskiNotlar) {
     return localStorageStateOku(key);
   } else {
-    return baslangicDegerleri
+    localStorageStateYaz(key, baslangicDegerleri);
+    return baslangicDegerleri;
+  }
+}
+
+export default function reducer(
+  state = baslangicNotlariniGetir(s10chLocalStorageKey),
+  action
+) {
+  switch (action.type) {
+    case NOT_EKLE:
+      const newNote = [action.payload, ...state.notlar];
+      const newState = { ...state, notlar: newNote };
+      localStorageStateYaz(s10chLocalStorageKey, newState);
+      return newState;
+
+    case NOT_SIL:
+      const notId = action.payload;
+      const deletedNotes = state.notlar.filter((not) => not.id !== notId);
+      const deletedState = { ...state, notlar: deletedNotes };
+      localStorageStateYaz(s10chLocalStorageKey, deletedState);
+      return deletedState;
+
+    default:
+      return state;
   }
 }
